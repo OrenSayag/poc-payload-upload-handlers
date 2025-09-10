6f63f4dc4015d95409343a02db9d298ed2578b75
Oren Sayag <orensayag500@gmail.com>
2025-09-10 15:51:15 +0300

    :wrench: media collection - added upload handlers

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,30 @@
 import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
+    slug: 'media',
+    access: {
+        read: () => true,
     },
-  ],
-  upload: true,
+    fields: [
+        {
+            name: 'alt',
+            type: 'text',
+            required: true,
+        },
+    ],
+    upload: {
+        handlers: [
+            async () => {
+                await new Promise((resolve) => setTimeout(resolve, 3_000))
+                console.log(`File processed after 3 seconds`)
+                return Response.json({ message: 'File processed after 3 seconds' })
+            },
+
+            async () => {
+                await new Promise((resolve) => setTimeout(resolve, 5_000))
+                console.log(`File processed after 5 seconds`)
+                return Response.json({ message: 'File processed after 8 seconds' })
+            },
+        ],
+    },
 }
